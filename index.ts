61ee14e79091002bf21a7f38660340b58e999e8d
TaimoorRasool2 <taimoor.admission@gmail.com>
2024-03-31 03:11:17 -0600

    atm machin compilation

---
+++ index.ts
@@ -0,0 +1,40 @@
+#!/usr/bin/env ts-node
+
+import inquirer from "inquirer";
+var myBalance = 10000;
+var myPinCode = 1234;
+var pinInput = await inquirer.prompt({
+  message: "please type your pin",
+  type: "number",
+  name: "pin",
+});
+
+if (pinInput.pin === myPinCode) {
+  var input = await inquirer.prompt({
+    message:
+      "what do you want to see either your available balance or want to withdraw",
+    type: "list",
+    name: "clientChoice",
+    choices: ["see balance", "withdraw Cash"],
+  });
+            if(input.clientChoice==="see balance"){
+                    console.log(`your availble balance is ${myBalance}`);
+            }
+            else if(input.clientChoice==="withdraw Cash"){
+                var amount= await inquirer.prompt({
+                    message:"please enter the amount",
+                    type :"number",
+                    name:"amountWithdraw"
+                })
+                if(amount.amountWithdraw<=myBalance){
+                    myBalance=myBalance-amount.amountWithdraw;
+                    console.log(`your have withdraw this ${amount.amountWithdraw} amount and remaining balance is ${myBalance}`);
+                }else{
+                    console.log("you don't have enough amount avaliable");
+                    
+                }
+            }
+
+} else {
+  console.log("your pin is wrong please try again");
+}
